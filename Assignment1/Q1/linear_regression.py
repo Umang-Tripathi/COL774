f6acc8a611eae4f4bb50f965927e557218c9538c
Umang Tripathi <umangtripathi@Umangs-MacBook-Pro.local>
2025-01-28 09:43:00 +0530

    intial directory structure

---
+++ Assignment1/Q1/linear_regression.py
@@ -0,0 +1,48 @@
+# Imports - you can add any other permitted libraries
+import numpy as np
+
+# You may add any other functions to make your code more modular. However,
+# do not change the function signatures (name and arguments) of the given functions,
+# as these functions will be called by the autograder.
+
+class LinearRegressor:
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y, learning_rate=0.01):
+        """
+        Fit the linear regression model to the data using Gradient Descent.
+        
+        Parameters
+        ----------
+        X : numpy array of shape (n_samples, n_features)
+            The input data.
+            
+        y : numpy array of shape (n_samples,)
+            The target values.
+
+        learning_rate : float
+            The learning rate to use in the update rule.
+            
+        Returns
+        -------
+        List of Parameters: numpy array of shape (n_iter, n_features,)
+            The list of parameters obtained after each iteration of Gradient Descent.
+        """
+        pass
+    
+    def predict(self, X):
+        """
+        Predict the target values for the input data.
+        
+        Parameters
+        ----------
+        X : numpy array of shape (n_samples, n_features)
+            The input data.
+            
+        Returns
+        -------
+        y_pred : numpy array of shape (n_samples,)
+            The predicted target values.
+        """
+        pass
